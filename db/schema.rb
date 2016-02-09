6d48f3abea018998d8f1b853c10313d0b1fa5624
Joey Drees <sammysosa21@fuse.net>
2016-02-08 19:14:53 -0500

    Added name columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206042831) do
+ActiveRecord::Schema.define(version: 20160209000536) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20160206042831) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
+    t.string   "username",               default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
