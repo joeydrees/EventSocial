055d1beebac30f9623a13ea0909fdba23049886b
Ryan Toler <rdt712@gmail.com>
2016-02-10 02:21:22 -0500

    Fixed log in form and user authentication features

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,8 @@
-class Users::SessionsController < Devise::RegistrationsController
+class Users::SessionsController < Devise::SessionsController
+  
   protected
     def after_sign_in_path_for(resource)
     	request.env['omniauth.origin'] || tweets_path(resource)
   	end
-
 
 end
