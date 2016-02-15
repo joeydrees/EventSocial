4c34b7092d8cf74a5f8d1d1377cbef0953c5c1f6
Ryan Toler <rdt712@gmail.com>
2016-02-15 15:39:35 -0500

    Added alerts and necessary files for configuration

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		
 		user = User.from_omniauth(request.env["omniauth.auth"])
 		if user.persisted?
+			flash[:notice] = "Signed in successfully!"
 			sign_in_and_redirect user
 		else
 			session["devise.user_attribute"] = user.attributes
