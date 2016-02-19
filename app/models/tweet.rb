838eb90b7c2620b521fd8ca4334e699c4d05297c
Ryan Toler <rdt712@gmail.com>
2016-02-19 01:36:18 -0500

    Cleaned up code

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,17 @@
 class Tweet < ActiveRecord::Base
+
+	def self.get_tweet(tweet)
+		where(id_str: tweet.id).first_or_create do |t|
+    		t.name =  tweet.user.name
+    		t.username = tweet.user.screen_name
+    		t.text = tweet.text
+ 			t.id_str = tweet.id
+   		end
+	end
+
+	def self.search(hashtag, count)
+		@search = $twitter.search("##{hashtag} -filter:retweets", lang: "en").take(count)
+		return @search
+	end
+
 end
