21afd42c577c42f127f5832802d40f63e389a6cf
Ryan Toler <rdt712@gmail.com>
2016-02-25 15:45:50 -0500

    Changed index in user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218163939) do
+ActiveRecord::Schema.define(version: 20160225192543) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20160218163939) do
     t.string   "profile_image",          default: ""
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email", "provider", "uid"], name: "index_users_on_identity", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
