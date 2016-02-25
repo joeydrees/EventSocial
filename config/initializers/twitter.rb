fabcd789ac32488d1ef3962d647893bc82f18ead
Ryan Toler <rdt712@gmail.com>
2016-02-25 16:54:01 -0500

    Added final changes for demo

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,17 @@
 # Put Twitter OAuth credentials here
 
-$twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+if Rails.env.developement?
+	$twitter = Twitter::REST::Client.new do |config|
+	  config.consumer_key = ENV["DEV_TWITTER_CONSUMER_KEY"]
+	  config.consumer_secret = ENV["DEV_TWITTER_CONSUMER_SECRET"]
+	  config.access_token = ENV["DEV_TWITTER_ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["DEV_TWITTER_ACCESS_TOKEN_SECRET"]
+	end
+elsif Rails.env.production?
+	$twitter = Twitter::REST::Client.new do |config|
+	  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+	  config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+	  config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	end
 end
