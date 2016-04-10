b87e6664aa469694a212bf5092ec4a090a70789c
Ryan Toler <rdt712@gmail.com>
2016-04-10 19:16:28 -0400

    Add nested comments helper for mobile

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,4 +4,11 @@ module CommentsHelper
       render(comment) + content_tag(:div, "", :id => "comment-#{comment.id}", :style => "display: none") + content_tag(:div, nested_comments(sub_comments), :class => "nested-comments")
     end.join.html_safe
   end
+
+  def nested_comments_mobile(comments)
+    comments.map do |comment, sub_comments|
+      render(comment) + content_tag(:div, "", :id => "comment-#{comment.id}-mobile", :style => "display: none") + content_tag(:div, nested_comments_mobile(sub_comments), :class => "nested-comments")
+    end.join.html_safe
+  end
+
 end
